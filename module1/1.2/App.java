eeecbceb870118f58883292a0e9c0b1a90fcc6e4
evgeny <ukal.evgen@gmail.com>
2023-02-09 23:43:24 +0500

    1.2 rdy

---
+++ module1/1.2/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        int a = input.nextInt();
+
+        System.out.println(a % 10);
+    }
+}
