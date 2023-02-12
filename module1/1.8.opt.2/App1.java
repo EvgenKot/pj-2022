8e03b7d3dfcaa53db5193bc89636cb53f9858c5c
EvgenKot <ukal.evgen@gmail.com>
2023-02-12 15:37:47 +0500

    1.8.opt.2

--- module1/1.8.opt.2/App1.java
+++ module1/1.8.opt.2/App1.java
@@ -3,16 +3,34 @@ import java.util.Scanner;
 public class App1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+        int n = in.nextInt();
 
-        int[] array = new int[N];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = in.nextInt();
-        }
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+
         in.close();
 
-        for (int i = 0; i < array.length; i += 2) {
-            System.out.print(array[i] + " ");
+        int[] m = new int[n + 1];
+
+        for (int i = 1; i <= n; i++)
+            m[i] = i;
+
+        for (; a < b; a++, b--) {
+            int temp = m[a];
+            m[a] = m[b];
+            m[b] = temp;
+
         }
+        for (; c < d; c++, d--) {
+            int temp = m[c];
+            m[c] = m[d];
+            m[d] = temp;
+
+        }
+        for (int i = 1; i <= n; i++)
+            System.out.printf("%d ", m[i]);
+        in.close();
     }
 }
