87ca19634fb2696c7d2447126e703910e65a5f24
EvgenKot <ukal.evgen@gmail.com>
2023-02-12 16:06:08 +0500

    1.9.opt naming

--- module1/1.9.opt/App2.java
+++ module1/1.9.opt/App2.java
@@ -1,15 +1,15 @@
-public class App2 {
+public class MyMath {
     public static int min(int a, int b, int c, int d) {
-            int min = a;
-            if (b < min) {
-                min = b;
-            }
-            if (c < min) {
-                min = c;
-            }
-            if (d < min) {
-                min = d;
-            }
-            return min;
+        int min = a;
+        if (b < min) {
+            min = b;
         }
+        if (c < min) {
+            min = c;
+        }
+        if (d < min) {
+            min = d;
+        }
+        return min;
+    }
 }
