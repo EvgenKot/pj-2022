799397ae810591862d7e7cb7b4a58a0efb096358
EvgenKot <ukal.evgen@gmail.com>
2023-02-12 12:11:34 +0500

    1.7

--- module1/1.7/App3.java
+++ module1/1.7/App3.java
@@ -3,21 +3,17 @@ import java.util.Scanner;
 public class App3 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            int counter = 0;
-            int answ;
-            int D = 0;
-            int U = 0;
-            while (true) {
-                answ = scanner.nextInt();
-                if ((answ < 100) & (answ > 9)) {
-                    D = answ / 10;
-                    U = answ % 10;
-                    counter += D + U;
-                } else {
+            int sum = 0;
+            while (scanner.hasNext()) {
+                int number = scanner.nextInt();
+                if (number < 10 || number > 99) {
                     break;
                 }
+                sum += number % 10;
+                sum += (number - (number % 10)) / 10;
+
             }
-            System.out.println(counter);
+            System.out.println(sum);
         }
     }
 }
--- module1/1.7/App4.java
+++ module1/1.7/App4.java
@@ -2,24 +2,16 @@ import java.util.Scanner;
 
 public class App4 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String str = in.nextLine();
-        String[] splitStr = str.split(" ");
-        int A = Integer.parseInt(splitStr[0]);
-        int B = Integer.parseInt(splitStr[1]);
-        int quotient = 0;
-        int rest;
-        while (true) {
-            rest = A - B;
-            A -= B;
-            if (rest >= 0)
-                quotient += 1;
-            else {
-                rest += B;
-                break;
+        try (Scanner scanner = new Scanner(System.in)) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int count = 0;
+
+            while (A >= B) {
+                count++;
+                A = A - B;
             }
+            System.out.println(count + " " + A);
         }
-        System.out.printf("%d %d", quotient, rest);
-        in.close();
     }
 }
