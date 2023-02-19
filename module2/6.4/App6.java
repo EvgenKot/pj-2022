8c1f36bdc5af8397ac95fa137e4106b58352536a
MurphyBLG <erwin16@ya.ru>
2023-02-19 17:30:10 +0500

    6.4

---
+++ module2/6.4/App6.java
@@ -0,0 +1,17 @@
+public class App6 {
+    public static void main(String[] args) {
+        
+    }    
+}
+
+class Cat implements Animal {
+    public String speak() {
+        return "Miy";
+    }
+}
+
+class Dog implements Animal {
+    public String speak() {
+        return "Gav";
+    }
+}
